f21f4981dcde092bc7bd86401e955806a5596c74
Harry Jackson <hmjackson@live.co.uk>
2025-08-30 07:02:44 +0100

    Further report drafting

--- game/collision.lua
+++ game/collision.lua
@@ -1,7 +1,7 @@
 collides = function(body1, body2)
 
-   return (abs(body1.x - body2.x) < (body1.width / 2) + (body2.width / 2) and
-           abs(body1.y - body2.y) < (body1.height / 2) + (body2.height / 2) and
-           abs(body1.z - body2.z) < (body1.depth / 2) + (body2.depth / 2))
+   return abs(body1.x - body2.x) < (body1.width + body2.width) /2  and
+          abs(body1.y - body2.y) < (body1.height + body2.height) / 2 and
+          abs(body1.z - body2.z) < (body1.depth + body2.depth) / 2
 
 end
